80181ecdaddff768023c36c920b9de54583353ba
dkrichins208@gmail.com <dkrichins@gmail.com>
2019-01-09 20:39:00 -0700

    initial setup

--- oAuthClient.js
+++ oAuthClient.js
@@ -1,8 +1,8 @@
 var OAuthClient = require('intuit-oauth')
 
 module.exports = new OAuthClient({
-    clientId: 'Q0FKrAYW5ORFZIoN4uF97sNxCu8RT5Abwy1Jl6LGEUaLy5Wtl7',
-    clientSecret: 'Kc3W9napNn574dWB7sdc4gH7kIJRW9BN9idicLVK',
+    clientId: 'Q0pgNhbEl9Oq5j00YvsLSx2Wba9khadE6XBarRjl3mbn1xasbJ',
+    clientSecret: 'ecE5YlMha7KaiipKbCOzLG96pMCtu10Xnd7F5kwS',
     environment: 'sandbox',
     redirectUri: 'http://localhost:3000/callback'
 });
