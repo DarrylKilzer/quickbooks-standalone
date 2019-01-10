a68c105305e38175d9002f9bb0209edff79f2091
dkrichins208@gmail.com <dkrichins@gmail.com>
2019-01-09 20:49:45 -0700

    dchanges

--- oAuthClient.js
+++ oAuthClient.js
@@ -4,5 +4,5 @@ module.exports = new OAuthClient({
     clientId: 'Q0pgNhbEl9Oq5j00YvsLSx2Wba9khadE6XBarRjl3mbn1xasbJ',
     clientSecret: 'ecE5YlMha7KaiipKbCOzLG96pMCtu10Xnd7F5kwS',
     environment: 'production',
-    redirectUri: 'http://localhost:3000/callback'
+    redirectUri: 'https://gatbuilder.herokuapp.com/callback'
 });
