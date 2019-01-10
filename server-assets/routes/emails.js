a503232e56f20518dfa783bf0520549d0af5e6c5
dkrichins208@gmail.com <dkrichins@gmail.com>
2019-01-09 21:15:56 -0700

    changes

--- server-assets/routes/emails.js
+++ server-assets/routes/emails.js
@@ -3,7 +3,7 @@ let oauthClient = require('../../oAuthClient')
 let quickBooks = require('../../Quickbooks')
 
 router.post('/', (req, res, next) => {
-    quickBooks.qbo.emailInvoice(151, "darrylkilzer@gmail.com", (err, invoice) => {
+    quickBooks.qbo.emailInvoice(req.body.invoiceId, req.body.email, (err, invoice) => {
         if (invoice) {
             res.send(invoice)
         } else {
